037a13b992cc5eb5dfec4e2c66fef014ca7c2536
West-wise <psh0036@naver.com>
2026-02-05 22:51:48 +0900

    feat: HMAC계산에 필요한 파라미터 추가

--- include/PacketUtil.hpp
+++ include/PacketUtil.hpp
@@ -8,11 +8,8 @@
 namespace SST {
     class PacketUtil {
     public:
-        // [Migration v2.0] Little Endian (Host Order) 준수
-        // 별도의 엔디안 변환 없이 직렬화/역직렬화 수행
-        
-        static std::vector<uint8_t> createPacket(uint16_t cmd_mask, uint32_t req_id, const std::vector<uint8_t>& body);
-        static std::vector<uint8_t> createErrorResponse(uint32_t req_id);
+        static std::vector<uint8_t> createPacket(uint16_t cmd_mask, uint32_t req_id, 
+            const std::vector<uint8_t>& body, const std::string& key);
     };
 }
 
